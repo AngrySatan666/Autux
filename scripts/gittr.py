eade1687d83d93f8a4459a1b3d52a511036c4edc
怒り の 悪魔 <142706418+AngrySatan666@users.noreply.github.com>
2025-05-30 01:17:03 -0400

    fast

--- scripts/gittr.py
+++ scripts/gittr.py
@@ -4,8 +4,10 @@ import subprocess
 raw_dir = os.path.dirname(os.path.abspath(__file__))
 _dir = os.path.dirname(raw_dir)
 
-add = subprocess.run(["git", "add", "."], cwd=_dir)
-commit = subprocess.run(["git", "commit", "-m", "fast"], cwd=_dir)
+add = subprocess.run(["git", "add", "."], capture_output=True, text=True, cwd=_dir)
+print(add.stdout.strip())
+commit = subprocess.run(["git", "commit", "-m", "fast"], capture_output=True, text=True, cwd=_dir)
+print(commit.stdout.strip())
 push = subprocess.run(["git", "push"], capture_output=True, text=True, cwd=_dir)
 print(push.stdout.strip())
 
